3b5f57770462157184468c98b987776289a694c4
CoolDude2349 <rer176021@gmail.com>
2025-03-12 21:21:24 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -2,9 +2,9 @@ self.__uv$config = {
   prefix: "/~/uv/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: "/uv/uv.handler.js",
-  client: "/uv/uv.client.js",
-  bundle: "/uv/uv.bundle.js",
-  config: "/uv/uv.config.js",
+  handler: "https://cdn.jsdelivr.net/gh/CoolDude2349/Test1@main/uv/uv.handler.js",
+  client: "https://cdn.jsdelivr.net/gh/CoolDude2349/Test1@main/uv/uv.client.js",
+  bundle: "https://cdn.jsdelivr.net/gh/CoolDude2349/Test1@main/uv/uv.bundle.js",
+  config: "https://cdn.jsdelivr.net/gh/CoolDude2349/Test1@main/uv/uv.config.js",
   sw: "/uv/uv.sw.js",
 };
